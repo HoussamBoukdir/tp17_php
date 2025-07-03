f89a089ac94156ff1dc44b91f90ee30ebcb1c60f
HoussamBoukdir <boukdir.houssam166@gmail.com>
2025-07-03 02:48:11 +0100

    Add files via upload

---
+++ les-boucles.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $filieres=['Developpement', 'Reseaux', 'Gestion', 'commerce'];
+    $n=count($filieres);
+
+    ?>
+    <h3>les listes des filiêres</h3>
+    <?php
+   /* for ($i=0; $i <$n ; $i++):
+    ?>
+    <a href="#"><?= $filieres[$i]?></a> <br> */
+    
+    $i=0;
+    do{
+        echo "<a href="#""
+    }
+    <?php endwhile ?>
+</body>
+</html>
